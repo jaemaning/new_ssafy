36ae20ff781d42a03feb28016bcac09f09170508
jaemaning <lovesay00@naver.com>
2023-02-22 00:27:57 +0900

    백조의 호수

--- 02_ssafy/0221/boj3197.py
+++ 02_ssafy/0221/boj3197.py
@@ -1,32 +1,33 @@
 # boj 3197 - 백조의 호수
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 def bird_bfs(que):
 
     memo = deque()
 
     # 백조 만나지는지 아래 ice_bfs 함수에서 day 마다 검사할 예정
+    # 처음은 초기 위치 que 가 들어오고 다음부턴 memo가 들어옴
     while que:
-        lenq = len(que)
+        x, y = que.popleft()
 
-        for _ in range(lenq):
-            x, y = que.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
+            # 만났으면 리턴
+            if 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == 'L':
+                return 1
 
-                # 만났으면 리턴
-                if 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == 'L':
-                    return 1
+            # 길이라면 T 로 길을 변경하며 나가고
+            if 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == ".":
+                lake[nx][ny] = "T"
+                que.append((nx, ny))
 
-                # 길이라면 T 로 길을 변경하며 나가고
-                if 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == ".":
-                    lake[nx][ny] = "T"
-                    que.append((nx, ny))
-
-                elif 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == "X":
-                    memo.append((nx, ny))
+            # 만약 물 옆에 있는 얼음 이라면? 다음번에 녹을 예정이니 memo 해둠
+            elif 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == "X":
+                memo.append((nx, ny))
 
     return memo
 
@@ -38,7 +39,15 @@ def ice_bfs(lake, memo):
     for i in range(R):
         for j in range(C):
             if lake[i][j] == ".":
-                q.append((i, j))
+                flag = 0
+                for k in range(4):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+
+                    if 0 <= nx < R and 0 <= ny < C and lake[nx][ny] == "X":
+                        flag = 1
+                if flag:
+                    q.append((i,j))
 
     day = 0
 
