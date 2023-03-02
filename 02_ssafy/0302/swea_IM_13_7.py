0260d5d26d6923620f57642985abbe277ef78b44
jaemaning <lovesay00@naver.com>
2023-03-02 21:30:58 +0900

    20230302 알고리즘 집 커밋

--- 02_ssafy/0302/swea_IM_13_7.py
+++ 02_ssafy/0302/swea_IM_13_7.py
@@ -3,18 +3,27 @@ import sys
 sys.stdin = open('input.txt','r')
 
 
-def perm(n,visited):
+def comb(n,visited):
+    global min_v
 
-    if len(rlt) == n:
-        result.append(rlt[:])
+    if len(rlt) == n//2:
+        first_x = first_y = second_x = second_y = 0
+        for i in range(n):
+            if visited[i]:
+                first_x += n_list[i][0]
+                first_y += n_list[i][1]
+            else:
+                second_x += n_list[i][0]
+                second_y += n_list[i][1]
+        min_v = min(min_v, (first_x-second_x)**2 + (first_y-second_y)**2)
         return
 
-    for i in range(n):
+    start = n_list.index(rlt[-1]) if rlt else 0
+    for i in range(start, n):
         if visited[i] == 0:
-            # print(visited)
             visited[i] = 1
             rlt.append(n_list[i])
-            perm(n, visited)
+            comb(n, visited)
             visited[i] = 0
             rlt.pop()
 
@@ -27,9 +36,10 @@ for tc in range(1, T+1):
     visited = [0] * n
     result = []
     rlt = []
+    min_v = 9999999999999999
 
     for _ in range(n):
         n_list.append(list(map(int,input().split())))
 
-    perm(n,visited)
-    print(result)
+    comb(n,visited)
+    print(f'#{tc} {min_v}')
